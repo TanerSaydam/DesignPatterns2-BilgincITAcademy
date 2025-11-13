27b66a57800ecf41e9dcc550a0b7c0a19f8d2d4f
Taner Saydam <taner_and_you@hotmail.com>
2025-11-13 14:45:18 +0300

    Builder Pattern 2

--- ClassicPatterns/01CreationalPatterns/04BuilderPattern/Program.cs
+++ ClassicPatterns/01CreationalPatterns/04BuilderPattern/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Builder Pattern");
+﻿using FluentEmail.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("Builder Pattern");
 
 #region Gerçek Hayat Örneği
 //House house = new(windowCount: 3, doorCount: 1, haveGarden: false, havePool: true);
@@ -22,6 +25,43 @@ var house3 = builder
     .HavePool()
     .Build();
 #endregion
+
+#region Proje Örneği
+new MailService()
+    .WithFrom("tanersaydam@gmail.com")
+    .WithTo("info@test.com")
+    .WithSubject("Test")
+    .WithBody("This is a test message")
+    .Send();
+
+new MailService()
+    .WithFrom("tanersaydam@gmail.com")
+    .WithTo("info@test2.com")
+    .WithSubject("Test")
+    .Send();
+
+new MailService()
+    .WithFrom("tanersaydam@gmail.com")
+    .WithTo("info@test2.com")
+    .WithSubject("Test")
+    .WithBody("This is a test message")
+    .WithAttachments(new List<string>() { "Hello", "World" })
+    .Send();
+#endregion
+
+ServiceCollection services = new();
+services
+    .AddFluentEmail("info@test.com")
+    .AddSmtpSender("localhost", 25);
+
+var srv = services.BuildServiceProvider();
+var fluentEmail = srv.GetRequiredService<IFluentEmail>();
+
+fluentEmail
+    .To("tanersaydam@gmail.com")
+    .Subject("Test")
+    .Body("<h1>Hello world!</h1>", true)
+    .Send();
 
 Console.ReadLine();
 
@@ -88,6 +128,64 @@ class HouseBuilder : IHouseBuilder
             doorCount: _doorCount,
             haveGarden: _haveGarden,
             havePool: _havePool);
+    }
+}
+#endregion
+
+#region Proje Örneği
+class MailService
+{
+    public MailService()
+    {
+    }
+    public MailService(string from, string to, string subject, string body, List<string> attachments)
+    {
+        From = from;
+        To = to;
+        Subject = subject;
+        Body = body;
+        Attachments = attachments;
+    }
+
+    public string From { get; set; } = default!;
+    public string To { get; set; } = default!;
+    public string Subject { get; set; } = default!;
+    public string Body { get; set; } = default!;
+    public List<string> Attachments { get; set; } = new();
+
+    public MailService WithFrom(string from)
+    {
+        From = from;
+        return this;
+    }
+
+    public MailService WithTo(string to)
+    {
+        To = to;
+        return this;
+    }
+
+    public MailService WithSubject(string subject)
+    {
+        Subject = subject;
+        return this;
+    }
+
+    public MailService WithBody(string body)
+    {
+        Body = body;
+        return this;
+    }
+
+    public MailService WithAttachments(List<string> attachments)
+    {
+        Attachments = attachments;
+        return this;
+    }
+
+    public void Send()
+    {
+
     }
 }
 #endregion
