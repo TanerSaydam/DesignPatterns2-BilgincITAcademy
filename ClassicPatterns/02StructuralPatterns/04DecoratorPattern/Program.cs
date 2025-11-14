cf96cc938ec4a93578ec3c0573996f5d3b5cfe9c
Taner Saydam <taner_and_you@hotmail.com>
2025-11-14 10:26:43 +0300

    Decorator Pattern

--- ClassicPatterns/02StructuralPatterns/04DecoratorPattern/Program.cs
+++ ClassicPatterns/02StructuralPatterns/04DecoratorPattern/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Decorator Pattern");
+
+IMailService service = new MailService();
+service = new LoggingMailDecorator(service);
+service = new SignatureMailDecorator(service);
+
+service.Send("info@taner.com", "Hello", " Hi, how are you?");
+
+Console.ReadLine();
+
+interface IMailService
+{
+    void Send(string to, string subject, string body);
+}
+
+class MailService : IMailService
+{
+    public void Send(string to, string subject, string body)
+    {
+        Console.WriteLine("[Mail Service] Sending email...\nTo: {0}\nSubject: {1}\nBody: {2}", to, subject, body);
+    }
+}
+
+abstract class MailDecorator : IMailService
+{
+    public IMailService _service;
+
+    protected MailDecorator(IMailService service)
+    {
+        _service = service;
+    }
+
+    public abstract void Send(string to, string subject, string body);
+}
+
+class LoggingMailDecorator : MailDecorator
+{
+    public LoggingMailDecorator(IMailService service) : base(service)
+    {
+    }
+
+    public override void Send(string to, string subject, string body)
+    {
+        Console.WriteLine("[LOG] Sending email...");
+        _service.Send(to, subject, body);
+    }
+}
+
+class SignatureMailDecorator : MailDecorator
+{
+    public SignatureMailDecorator(IMailService service) : base(service)
+    {
+    }
+
+    public override void Send(string to, string subject, string body)
+    {
+        body += "\n\nBest Regards!";
+        _service.Send(to, subject, body);
+    }
+}
--- ClassicPatterns/02StructuralPatterns/04DecoratorPattern/README.md
+++ ClassicPatterns/02StructuralPatterns/04DecoratorPattern/README.md
@@ -1,5 +1,3 @@
 # Decorator Pattern
 
-Bir class'ýn yaýsýný deðiþtirmeden ona dinamik olarak yeni davranýþ eklemeyi öneren pattern. Yani miras almak yerine mevcut nesneyi sarmallayarak geniþletmeyi amaçlar.
-
-//10:15 görüþelim
+Bir class'ýn yapýsýný deðiþtirmeden ona dinamik olarak yeni davranýþ eklemeyi öneren pattern. Yani miras almak yerine mevcut nesneyi sarmallayarak geniþletmeyi amaçlar.
